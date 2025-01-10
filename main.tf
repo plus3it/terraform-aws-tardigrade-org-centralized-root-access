0539971ce16c245ed4d8c04a63263ba5620c4852
Loren Gordon <lorengordon@users.noreply.github.com>
2025-01-10 14:37:49 -0800

    Validates that the provided credential is for the AWS Organization account

--- main.tf
+++ main.tf
@@ -3,6 +3,13 @@ resource "aws_iam_organizations_features" "this" {
     "RootCredentialsManagement",
     "RootSessions"
   ]
+
+  lifecycle {
+    precondition {
+      condition     = data.aws_organizations_organization.this.master_account_id == data.aws_caller_identity.this.account_id
+      error_message = "The AWS Organization account must be used to enable the AWS Organizations features. Please run this module with a provider configured for the AWS Organization account."
+    }
+  }
 }
 
 resource "aws_organizations_delegated_administrator" "this" {
@@ -11,3 +18,6 @@ resource "aws_organizations_delegated_administrator" "this" {
   account_id        = var.org_centralized_root_access.delegated_administrator.account_id
   service_principal = "iam.amazonaws.com"
 }
+
+data "aws_organizations_organization" "this" {}
+data "aws_caller_identity" "this" {}
